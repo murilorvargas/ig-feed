50d1e3935cf2f542807e258ac0783175369e96c7
Murilo Vargas <muriraubach@gmail.com>
2022-06-25 18:40:54 -0300

    refactor: use avatar component instead of image tag in sidebar component

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { PencilLine } from "phosphor-react";
 
+import Avatar from '../Avatar';
+
 import styles from "./styles.module.css";
 
 function Sidebar() {
@@ -11,7 +13,7 @@ function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
       />
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/murilorvargas.png" />
+        <Avatar src="https://github.com/murilorvargas.png" />
 
         <strong>Murilo Vargas</strong>
         <span>Software Developer</span>
