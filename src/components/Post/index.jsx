86e431dd019da0c5a8f13ba372f9813bb08e9873
Murilo Vargas <muriraubach@gmail.com>
2022-07-08 16:11:49 -0300

    feat: add post component properties

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,40 +1,54 @@
-import React from 'react';
+import React from "react";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
 
-import Avatar from '../Avatar';
-import Comment from './Comment';
+import Avatar from "../Avatar";
+import Comment from "./Comment";
 
 import styles from "./styles.module.css";
 
-function Post() {
+function Post({ content, author, publishedAt }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/murilorvargas.png" />
+          <Avatar src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>Murilo Vargas</strong>
-            <span>Software Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          Publicado {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉{" "}<a href="">jane.design/doctorcare</a></p>
-        <p>
-          <a href="">#novoprojeto</a>{" "}
-          <a href="">#nlw</a>{" "}
-          <a href="">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if (line.type === "paragraph") 
+            return <p>{line.content}</p>;
+          else if (line.type === "link") 
+            return <p><a href="">{line.content}</a></p>;
+        })}
       </div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea 
+        <textarea
           placeholder="Deixe um comentário"
         />
 
